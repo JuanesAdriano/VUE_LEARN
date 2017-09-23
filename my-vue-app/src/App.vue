d3ce6cec4b8a6f643f9c4662048c895538164149
JuanesAdriano <juaneshtk50@gmail.com>
2017-09-22 22:44:46 -0300

    Initial commit

---
+++ my-vue-app/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div id="app">
+    <my-menu title="Primeira aplicação" teste="na parte de componente"></my-menu>
+        <h1>{{ msg }}</h1>
+
+    </div>
+</template>
+
+<script>
+    import myMenu from "./myMenu.vue";
+
+    export default {
+        components: {
+            myMenu
+        },
+
+        data () {
+            return {
+                msg: 'Hoje é dia de miça doidho, ce góspeu e pá na pas de Jová vei doido'
+            }
+        }
+    }
+</script>
+
+<style>
+    body {
+        font-family: Helvetica, sans-serif;
+    }
+</style>
---
+++ my-vue-app/src/myMenu.vue
@@ -0,0 +1,10 @@
+<template>
+    <h4> {{title}} e {{teste}} </h4>
+</template>
+<script>
+
+    export default{
+        props: ['title', 'teste']
+
+    }
+</script>
